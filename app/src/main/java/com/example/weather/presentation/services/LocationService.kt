5a73889333a7b6f1856f89c3b00ef2756b95abcc
YamamotoTsunetomo <arlongtwentylemons@gmail.com>
2022-06-10 11:50:25 +0400

    add permission check to location services

--- app/src/main/java/com/example/weather/presentation/services/LocationService.kt
+++ app/src/main/java/com/example/weather/presentation/services/LocationService.kt
@@ -1,13 +1,15 @@
 package com.example.weather.presentation.services
 
-import android.annotation.SuppressLint
+import android.Manifest
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.IBinder
+import androidx.core.app.ActivityCompat
 
 class LocationService : Service(), LocationListener {
 
@@ -21,18 +23,23 @@ class LocationService : Service(), LocationListener {
 
     override fun onBind(intent: Intent?): IBinder? = null
 
-    @SuppressLint("MissingPermission")
     fun getLocation(context: Context): Location? {
         locationManager = context
             .getSystemService(LOCATION_SERVICE) as LocationManager
         return if (isNetworkEnabled) locationManager?.let {
 
-            it.requestLocationUpdates(
-                LocationManager.NETWORK_PROVIDER,
-                MIN_UPDATE_TIME,
-                MIN_UPDATE_DISTANCE,
-                this
-            )
+            val hasPermission = ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+
+            if (hasPermission)
+                it.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    MIN_UPDATE_TIME,
+                    MIN_UPDATE_DISTANCE,
+                    this
+                )
 
             it.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
         } else null
